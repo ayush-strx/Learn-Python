4aa5b1cf2f2e997d277eb6cd994e894d0ad3e9f6
ayush-strx <ayushsanjayt@gmail.com>
2026-07-15 15:13:18 +0530

    Currency_Notes_Calculator

--- Mini projects/Currency_Notes_Calculator.py
+++ Mini projects/Currency_Notes_Calculator.py
@@ -1,6 +1,6 @@
 amount = int(input("Enter Your Amount:"))
 
-print("----------------Currency Notes Calculator------------")
+print("----------------Currency Notes Calculator---------------")
 print("1 - 100 Rupee Notes")
 print("2 - 200 Rupee Notes")
 print("3 - 500 Rupee Notes")
@@ -9,21 +9,18 @@ notes = int(input("Enter Your Choice:"))
 
 if notes == 1:
     a  = (print(" Total notes:", amount//100))
-    print(a)
-    
+
     remain = (amount%100)
     print("Remaining Amount:", remain)
 
 elif notes == 2:
     a  = (print(" Total notes:", amount//200))
-    print(a)
-    
+
     remain = (amount%200)
     print("Remaining Amount:", remain)
 
 elif notes == 3:
     a  = (print(" Total notes:", amount//500))
-    print(a)
     
     remain = (amount%500)
     print("Remaining Amount:", remain)
